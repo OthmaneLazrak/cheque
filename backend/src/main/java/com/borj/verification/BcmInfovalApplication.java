19c924b77bd26ddd2c48a15f7bf35d4005eebf69
OthmaneLazrak <othmane.lazrek@gmail.com>
2026-08-27 11:24:10 +0100

    first commit

---
+++ backend/src/main/java/com/borj/verification/BcmInfovalApplication.java
@@ -0,0 +1,13 @@
+package com.borj.verification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcmInfovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcmInfovalApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/borj/verification/BcmInfovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borj.verification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcmInfovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
